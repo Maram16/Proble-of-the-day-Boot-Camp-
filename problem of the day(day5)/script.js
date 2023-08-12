956ea315ee2bdba34e96bf476ed80f447e80dcdd
Maram16 <141616598+Maram16@users.noreply.github.com>
2023-08-12 20:28:58 +0400

    Add files via upload

---
+++ problem of the day(day5)/script.js
@@ -0,0 +1,16 @@
+
+a=[1,2,2,8]
+b=[2,4,6,12]
+counter = 0
+
+  for(n=0; n<=3; n++){
+       if(( a[n] / b[n] + a[n] / b[n]) == 1){
+         counter++
+        
+       }
+
+    }
+  
+
+console.log(counter)
+

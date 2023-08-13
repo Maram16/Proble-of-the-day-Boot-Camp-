e1c6bb4f52923a05b33f42b8ff2f9e6ab18a1433
Maram16 <141616598+Maram16@users.noreply.github.com>
2023-08-13 23:59:39 +0400

    Add files via upload

---
+++ problem of the day(day6)/script.js
@@ -0,0 +1,10 @@
+
+const dice1 = Math.floor(Math.random()*6+1)
+const dice2= Math.floor(Math.random()*6+1)
+
+if (dice1>dice2){
+    console.log("the turn is good")
+    
+}else{
+    console.log("the turn is bad ")
+}

fb4e274646d25563c6833fc9ce6f883cce8757fa
Maram16 <141616598+Maram16@users.noreply.github.com>
2023-08-19 21:56:15 +0400

    Add files via upload

---
+++ question of the day(day8)/script.js
@@ -0,0 +1,14 @@
+getnum(2, 5);
+arr = []
+const getRev=(n)=>{
+    for (let i=0; i<=n; i++){
+        let n = prompt("enter a number")
+        arr.push(n)
+
+
+}
+    const rev = arr.reverse()
+    console.log(rev)
+}
+
+getRev(4)
